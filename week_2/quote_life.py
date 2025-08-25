5c93666ea58fa2944de1239ac6ccfc425ac6dba7
Leslie Khattarchebli <lkhattarchebli@my365.bellevue.edu>
2025-08-24 20:53:41 -0700

    updated

--- week_2/quote_life.py
+++ week_2/quote_life.py
@@ -8,3 +8,13 @@ Description: This program prints a quote and gives credit the author.
 # Print the quote and author
 print("All I know is that I know nothing")
 print("- Socrates")
+
+# Importing required module (normal level comment explaining the following code block)
+import os
+# Getting the current working directory (normal level comment explaining the following code block)
+current_dir = os.getcwd()
+ 
+
+# Printing the current working directory
+
+print(current_dir)
