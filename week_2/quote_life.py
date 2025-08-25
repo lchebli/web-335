392a4aa7e6b0598915fbe9f8987a01c55535eb8c
Leslie Khattarchebli <lkhattarchebli@my365.bellevue.edu>
2025-08-24 20:34:43 -0700

    1.1Exercise

---
+++ week_2/quote_life.py
@@ -0,0 +1,10 @@
+"""
+Author: Leslie Khattarcheblli
+Date: August 24, 2025
+Title: quote_life.py
+Description: This program prints a quote and gives credit the author.
+"""
+
+# Print the quote and author
+print("All I know is that I know nothing")
+print("- Socrates")
